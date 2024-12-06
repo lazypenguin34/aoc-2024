6d30a5dab409476e188f1012c008f6d590a8fdc7
Nathan Karwel <115108705+lazypenguin34@users.noreply.github.com>
2024-12-05 21:16:57 -0500

    day 5 part 1

---
+++ day5.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool passesPartOne(const unordered_map<int, vector<int>> &rules, const vector<int> &update) {
+    for (int i=0;i<update.size();i++) {
+        int x = update[i];
+        if (rules.find(x) == rules.end()) {
+            continue; // if there's no rules for a character, we can skip it
+        }
+
+        vector<int> specificRules = rules.at(x);
+        for (int y : specificRules) {
+            auto it = find(update.begin(), update.end(), y);
+            if (it == update.end()) {
+                continue; // if y isn't in the update, then it can't have broken the rule
+            }
+
+            int index = distance(update.begin(), it);
+            if (index < i) { // then we have broke the rule
+                return false;
+            }
+        }
+
+    }
+
+    return true;
+}
+
+int main() {
+    // Get input
+    unordered_map<int, vector<int>> rules;
+    vector<vector<int>> updates;
+    string line;
+    while(getline(cin, line) && !line.empty()) {
+        int x = stoi(line.substr(0, 2));
+        int y = stoi(line.substr(3, 2));
+
+        rules[x].push_back(y);
+    }
+
+    while(getline(cin, line) && !line.empty()) {
+        vector<int> update;
+        for (int i=0;i<line.length();i += 3) {
+            int x = stoi(line.substr(i, 2));
+            update.push_back(x);
+        }
+        updates.push_back(update);
+    }
+
+    // Part 1
+    int total = 0;
+    for(vector<int> update : updates) {
+        if (passesPartOne(rules, update)) {
+            total += update[update.size() / 2]; // idk if the -1 is neccessary but im doing it anyway
+        }
+    }
+    printf("Part 1: %d\n", total); // gonna start using printf now cause internet man said cout is ass (it is)
+}
